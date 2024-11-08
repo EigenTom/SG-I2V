1b336b8f26668734fc308c0e6dc0425212df7b83
Kmcode1 <kmcodetk@gmail.com>
2024-11-08 07:26:37 -0500

    upd

--- inference.sh
+++ inference.sh
@@ -1,8 +1,7 @@
 #Run sh ./inference.sh
 for f in ./examples/*; do
     if [ -d "$f" ]; then
-        # Will not run if no directories are available
         folder=$(basename -- "$f")
-        CUDA_VISIBLE_DEVICES=8 python inference.py --input_dir "$f" --output_dir ./output/"$folder"
+        CUDA_VISIBLE_DEVICES=0 python inference.py --input_dir "$f" --output_dir ./output/"$folder"
     fi
 done
